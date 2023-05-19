bc31de95d2b034f4eaba24c7522081eabd2cb89c
Ilya Klimenyuk <ilya.klimenyuk@gmail.com>
2023-05-19 14:24:40 +0300

    SPT-1347 update Example project

--- Example/Example/ViewController.swift
+++ Example/Example/ViewController.swift
@@ -16,6 +16,8 @@ final class ViewController: UIViewController {
         DebugScreenConfiguration.shared.selectServerActionsProvider = ServersProvider()
         DebugScreenConfiguration.shared.featureToggleActionsProvider = FeatureToggleProvider()
         DebugScreenConfiguration.shared.selectableTextProvider = TextsProvider()
+
+        DebugScreenConfiguration.shared.logCatcherService.start()
     }
 
 }
@@ -29,6 +31,7 @@ final class ActionsProviderExample: ActionsProvider {
         let modelForSecondAction = ActionModel(title: "Second action", block: {
             print("perform second clear action")
         })
+
         return [
             ActionsProviderModel(
                 header: "Header of first action",
@@ -95,46 +98,52 @@ enum BusinessFeatureToggle {
 
 final class FeatureToggleProvider: FeatureToggleActionsProvider {
 
-    private var featureActions = [
-        FeatureToggleModel(text: FeatureToggleKey.feature1.rawValue, value: true),
-        FeatureToggleModel(text: FeatureToggleKey.feature2.rawValue, value: false),
-        FeatureToggleModel(text: FeatureToggleKey.feature3.rawValue, value: true),
-        FeatureToggleModel(
-            text: FeatureToggleKey.business1.rawValue,
-            value: BusinessFeatureToggle.isPushNotificationsAvailable
-        )
-    ]
+    private var featureActions: [FeatureToggleModel] = []
 
     func actions() -> [FeatureToggleModel] {
+        let featureActions = [
+            FeatureToggleModel(text: FeatureToggleKey.feature1.rawValue, value: true) { [weak self] newValue in
+                self?.updateModel(with: FeatureToggleKey.feature1.rawValue, newValue: newValue)
+                self?.doAction1()
+            },
+            FeatureToggleModel(text: FeatureToggleKey.feature2.rawValue, value: false) { [weak self] newValue in
+                self?.updateModel(with: FeatureToggleKey.feature2.rawValue, newValue: newValue)
+                self?.doAction2()
+            },
+            FeatureToggleModel(text: FeatureToggleKey.feature3.rawValue, value: true) { [weak self] newValue in
+                self?.updateModel(with: FeatureToggleKey.feature3.rawValue, newValue: newValue)
+                self?.doAction3()
+            },
+            FeatureToggleModel(
+                text: FeatureToggleKey.business1.rawValue,
+                value: BusinessFeatureToggle.isPushNotificationsAvailable
+            ) { [weak self] newValue in
+                self?.updateModel(with: FeatureToggleKey.business1.rawValue, newValue: newValue)
+                BusinessFeatureToggle.isPushNotificationsAvailable = newValue
+            }
+        ]
+
+        self.featureActions = featureActions
+
         return featureActions
     }
 
-    func handleAction(with text: String, newValue: Bool) {
-        guard let featureToggle = FeatureToggleKey(rawValue: text) else {
+    func updateModel(with text: String, newValue: Bool) {
+        guard
+            let featureToggle = FeatureToggleKey(rawValue: text),
+            let featureIndex = featureActions.firstIndex(where: { $0.text == featureToggle.rawValue })
+        else {
             return
         }
 
-        if let featureIndex = featureActions.firstIndex(
-            where: { $0.text == featureToggle.rawValue }
-        ) {
-            featureActions[featureIndex].value = newValue
-        }
-
-        switch featureToggle {
-        case .feature1:
-            doAction1()
-        case .feature2:
-            doAction2()
-        case .feature3:
-            doAction3()
-        case .business1:
-            BusinessFeatureToggle.isPushNotificationsAvailable = newValue
-        }
+        featureActions[featureIndex].value = newValue
     }
 
-    func doAction1() { }
-    func doAction2() { }
-    func doAction3() { }
+    func doAction1() {}
+
+    func doAction2() {}
+
+    func doAction3() {}
 
 }
 
