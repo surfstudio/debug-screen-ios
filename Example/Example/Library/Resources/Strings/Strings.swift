3c2e0e0f93fa336bfc03bc91bbf849e8605b5280
Ilya Klimenyuk <ilya.klimenyuk@gmail.com>
2023-06-01 23:39:15 +0300

    SPT-1471 update Example project resources

---
+++ Example/Example/Library/Resources/Strings/Strings.swift
@@ -0,0 +1,91 @@
+// swiftlint:disable all
+// Generated using SwiftGen — https://github.com/SwiftGen/SwiftGen
+
+import Foundation
+
+// swiftlint:disable superfluous_disable_command file_length implicit_return
+
+// MARK: - Strings
+
+// swiftlint:disable explicit_type_interface function_parameter_count identifier_name line_length
+// swiftlint:disable nesting type_body_length type_name vertical_whitespace_opening_braces
+internal enum L10n {
+
+  internal enum Actions {
+    /// Удаление
+    internal static let destructiveTitle = L10n.tr("Localizable", "Actions.destructiveTitle")
+    /// Действия
+    internal static let header = L10n.tr("Localizable", "Actions.header")
+    /// Вспомогательное
+    internal static let secondaryTitle = L10n.tr("Localizable", "Actions.secondaryTitle")
+  }
+
+  internal enum ActionsList {
+    /// Первое
+    internal static let firstActionTitle = L10n.tr("Localizable", "ActionsList.firstActionTitle")
+    /// Выберите действие
+    internal static let message = L10n.tr("Localizable", "ActionsList.message")
+    /// Второе
+    internal static let secondActionTitle = L10n.tr("Localizable", "ActionsList.secondActionTitle")
+    /// Третье
+    internal static let thirdActionTitle = L10n.tr("Localizable", "ActionsList.thirdActionTitle")
+    /// Список действий
+    internal static let title = L10n.tr("Localizable", "ActionsList.title")
+  }
+
+  internal enum CopiedText {
+    /// Ключ SSH
+    internal static let firstTextTitle = L10n.tr("Localizable", "CopiedText.firstTextTitle")
+    /// Копируемый текст
+    internal static let header = L10n.tr("Localizable", "CopiedText.header")
+    /// Токен
+    internal static let secondTextTitle = L10n.tr("Localizable", "CopiedText.secondTextTitle")
+    /// Данные для копирования
+    internal static let thirdTextTitle = L10n.tr("Localizable", "CopiedText.thirdTextTitle")
+  }
+
+  internal enum FeatureToggles {
+    /// Показывать касания
+    internal static let firstToggleTitle = L10n.tr("Localizable", "FeatureToggles.firstToggleTitle")
+    /// Переключатели
+    internal static let header = L10n.tr("Localizable", "FeatureToggles.header")
+    /// Использовать SSH
+    internal static let secondToggleTitle = L10n.tr("Localizable", "FeatureToggles.secondToggleTitle")
+    /// Открыть листинг foodtech/ecom каталога перед просмотром
+    internal static let thirdToggleTitle = L10n.tr("Localizable", "FeatureToggles.thirdToggleTitle")
+  }
+
+  internal enum ServerSelection {
+    /// Продовый
+    internal static let firstServerTitle = L10n.tr("Localizable", "ServerSelection.firstServerTitle")
+    /// Выбор сервера
+    internal static let header = L10n.tr("Localizable", "ServerSelection.header")
+    /// Тестовый
+    internal static let secondServerTitle = L10n.tr("Localizable", "ServerSelection.secondServerTitle")
+    /// Моковый (с очень очень очень длинным описанием)
+    internal static let thirdServerTitle = L10n.tr("Localizable", "ServerSelection.thirdServerTitle")
+  }
+}
+// swiftlint:enable explicit_type_interface function_parameter_count identifier_name line_length
+// swiftlint:enable nesting type_body_length type_name vertical_whitespace_opening_braces
+
+// MARK: - Implementation Details
+
+extension L10n {
+  private static func tr(_ table: String, _ key: String, _ args: CVarArg...) -> String {
+    let format = BundleToken.bundle.localizedString(forKey: key, value: nil, table: table)
+    return String(format: format, locale: Locale.current, arguments: args)
+  }
+}
+
+// swiftlint:disable convenience_type
+private final class BundleToken {
+  static let bundle: Bundle = {
+    #if SWIFT_PACKAGE
+    return Bundle.module
+    #else
+    return Bundle(for: BundleToken.self)
+    #endif
+  }()
+}
+// swiftlint:enable convenience_type
