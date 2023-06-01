64ff4bff09d9627d1b2712b08014857b90694698
Ilya Klimenyuk <ilya.klimenyuk@gmail.com>
2023-06-01 23:40:32 +0300

    SPT-1471 add UIKit extensions and UserDefaultsService for Example project

---
+++ Example/Example/Services/UserDefaultsService/UserDefaultsKeys.swift
@@ -0,0 +1,13 @@
+//
+//  UserDefaultsKeys.swift
+//  Example
+//
+//  Created by Ilya Klimenyuk on 31.05.2023.
+//
+
+enum UserDefaultsKeys {
+    static let isNeedShowEcomCatalog = "isNeedShowEcomCatalog"
+    static let isNeedShowTouches = "isNeedShowTouches"
+    static let isNeedUseSSH = "isNeedUseSSH"
+    static let serverUrl = "serverUrl"
+}
